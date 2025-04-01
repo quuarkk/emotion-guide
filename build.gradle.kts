031615272b0a5f22ca192a05220a90f60b780e49
Xpohoc28 <viktor.chaplygin1@gmail.com>
2025-04-01 07:46:14 +0300

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,17 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+    }
+    dependencies {
+        classpath ("com.android.tools.build:gradle:8.8.0")
+        classpath ("com.google.gms:google-services:4.4.2")
+    }
+}
 plugins {
     alias(libs.plugins.android.application) apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false
+//    id("com.google.gms.google-services")
 }
